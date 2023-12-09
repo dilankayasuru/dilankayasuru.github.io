d11d99cbad5133e4c4f8a2bafb9335feaf592c74
dilankaysuru <dilankaysuru001@gmail.com>
2023-12-10 01:22:11 +0530

    update booking page js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// --------------------------------------------------------------------------------------------------------------------
+// ************************************ Needed DOM Elements for hotel room booking ************************************
+// --------------------------------------------------------------------------------------------------------------------
 const hotelBookingForm = document.getElementById("booking_form");
 
 const roomTypeCheckbox = document.getElementsByName("room_type");
@@ -16,7 +19,6 @@ const loyaltyPointsOnForm = document.getElementById("loyaltyPoints");
 
 const showCurrentBooking = document.getElementById("outputTotal");
 
-
 // Other extra elements on HTML Form
 const hotelName = document.getElementById("stay");
 const checkInDate = document.getElementById("checkin");
@@ -27,8 +29,6 @@ const customerEmail = document.getElementById("user_email");
 const numOfAdults = document.getElementById("adult");
 const customerAddress = document.getElementById("address");
 
-
-
 // Elements on loylty popup window
 const bluredBackground = document.getElementById("bluredBackground");
 
@@ -37,7 +37,6 @@ const showCurrentLoyalty = document.getElementById("loyaltyPointsToearn");
 const showAvailableLoyalty = document.getElementById("pointsAvailable");
 const showTotalLoyalty = document.getElementById("totalLoyaltyPoints");
 const loyaltyWindowClose = document.getElementById("loyaltyClose");
-
 
 // Hotel Booking summary popup elements
 const hotelBookingSummaryPopup = document.getElementById("hotelConfirm");
@@ -58,12 +57,51 @@ const loyaltyPointsSummary = document.getElementById("loyaltyPointsOutput");
 
 const addToFavBtn = document.getElementById("addToFav");
 
-
 // Buttons on html
 const checkLoyaltyBtn = document.getElementById("loyaltyPointsCheck");
 const hotelBookNowBtn = document.getElementById("book_now");
 const applyFavouritesBtn = document.getElementById("applyFav");
 
+
+// ---------------------------------------------------------------------------------------------------------------------
+// ************************************* Needed DOM Elements for adventure booking *************************************
+// ---------------------------------------------------------------------------------------------------------------------
+// Adventure booking form
+const advBooking_form = document.getElementById("advBooking_form");
+
+// Adventure booking form input values for calculations
+const guideSelection = document.getElementsByName("guide");
+
+const advLocalAdultsNum = document.getElementById("adv_local_adults");
+const advLocalKidsNum = document.getElementById("adv_local_kids");
+const advforeignAdultsNum = document.getElementById("adv_foreign_adults");
+const advforeignKidsNum = document.getElementById("adv_foreign_kids");
+
+// other input values of adventure booking
+const advBookingName = document.getElementById("advBookingName");
+const advBookingPhone = document.getElementById("advBookingPhone");
+const advTime = document.getElementsByName("time");
+const advDate = document.getElementById("advDate");
+
+// Adventure Booking Buttons
+const advBookBtn = document.getElementById("adv_booking");
+const advApplyFav = document.getElementById("advApplyFav");
+
+// Adventure booking summary output elements
+const advCostDisplay = document.getElementById("advCost");
+
+const advConfirmPopup = document.getElementById("advConfirm");
+
+const advCustomerName = document.getElementById("advCustomerName");
+const advDateOutput = document.getElementById("advDateOutput");
+const timeSlotOutput = document.getElementById("timeSlotOutput");
+const advOutputSummary = document.getElementById("advOutputSummary");
+const advTotalOutput = document.getElementById("advTotalOutput");
+
+const advPopupClose = document.getElementById("done");
+const addToFavAdv = document.getElementById("addToFavAdv");
+
+// Variables for Hotel room booking
 let totalCost;
 let singelRoomNum;
 let doubleRoomNum;
@@ -80,11 +118,26 @@ const tripleRoomPrice = 40000;
 const extraBedPrice = 8000;
 const extraPriceForKids = 5000;
 
-const selectedExtraRequirements = [];
-const selectedRoomTypes = [];
+let selectedExtraRequirements;
+let selectedRoomTypes;
+
+// Variables for Adventure booking
+let guide;
+let advTotalCost;
+let advLocalAdults;
+let advLocalKids;
+let advForeignAdults;
+let advForeignKids;
+let selectedAdvTime;
+
+let localAdultsPrice;
+let localKidsPrice;
+let foreignAdultsPrice;
+let foreignKidsPrice;
 
 
 initiallise();
+initialliseAdvForm();
 
 
 // Update when room type is changed
@@ -119,14 +172,13 @@ loyaltyWindowClose.addEventListener("click", () => {
     bluredBackground.style.display = 'none';
 })
 
-
-// Form on submit
+// Hotel booking form on submit
 hotelBookNowBtn.addEventListener("click", function (event) {
     event.preventDefault();
-    if(validateForm()) {
+    if (validateForm()) {
         checkPromoCode();
         calculateHotelCost();
-    
+
         // Store Loyalty Points on local storage
         localStorage.setItem("loyalty", JSON.stringify(availableLoyaltyPoints + checkLoyalty()));
         showHotelBookingSummary();
@@ -160,10 +212,27 @@ function initiallise() {
     kids = 0;
     promoDiscount = 0;
 
+    selectedExtraRequirements = [];
+    selectedRoomTypes = [];
+
     loyaltyPointsOnForm.innerText = getLoyaltyPoints();
     showCurrentBooking.innerText = totalCost;
 }
 
+// Initialise adventure booking variables
+function initialliseAdvForm() {
+    guide = false;
+    advTotalCost = 0;
+    advLocalAdults = 0;
+    advLocalKids = 0;
+    advForeignAdults = 0;
+    advForeignKids = 0;
+    localAdultsPrice = 5000;
+    localKidsPrice = 2000;
+    foreignAdultsPrice = 10000;
+    foreignKidsPrice = 5000;
+    selectedAdvTime = "";
+}
 
 // Calculate total cost of booking
 function calculateHotelCost() {
@@ -463,8 +532,8 @@ function validateForm() {
     let userNumber = customerPhone.value;
     let roomType = validateRoomType();
     let adults = numOfAdults.value;
-    let checkIn = checkInDate.value;
-    let checkOut = checkOutDate.value;
+    const checkIn = new Date(checkInDate.value);
+    const checkOut = new Date(checkOutDate.value);
 
     if (!(userName)) {
         alert("Please enter your Name");
@@ -476,10 +545,12 @@ function validateForm() {
         alert("Please select a room type");
     } else if (!(adults) || adults == 0) {
         alert("Please enter number of adults");
-    } else if (!(checkIn)) {
-        alert("Please enter check in date");
-    } else if (!(checkOut)) {
-        alert("Please enter check out date");
+    } else if (!(checkIn > new Date())) {
+        alert("Please enter a valid check in date");
+    } else if (!(checkOut > new Date())) {
+        alert("Please enter a valid check out date");
+    } else if (checkIn >= checkOut) {
+        alert("Check out date can not be older than check in date");
     } else {
         validated = true;
         console.log("Validation Done!");
@@ -497,4 +568,270 @@ function validateRoomType() {
         }
     })
     return roomtypeSelected;
+}
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// *********************************Adventure Booking Calculations starting from here *********************************
+// --------------------------------------------------------------------------------------------------------------------
+
+
+// Check whether if the guide was selected
+guideSelection.forEach(element => element.addEventListener("change", () => {
+    if (element.checked) {
+        if (element.value === "withGuide") {
+            localAdultsPrice = 6000;
+            localKidsPrice = 2500;
+            foreignAdultsPrice = 11000;
+            foreignKidsPrice = 5500;
+            console.log("Checked")
+            guide = true;
+            calculateAdvCost();
+        } else {
+            localAdultsPrice = 5000;
+            localKidsPrice = 2000;
+            foreignAdultsPrice = 10000;
+            foreignKidsPrice = 5000;
+            console.log("unchecked")
+            guide = false;
+            calculateAdvCost();
+        }
+    }
+}));
+
+// Check the number of local adults and update the total 
+advLocalAdultsNum.addEventListener("change", calculateAdvCost);
+
+// Check the number of local kids and update the total 
+advLocalKidsNum.addEventListener("change", calculateAdvCost);
+
+// Check the number of foreign adults and update the total 
+advforeignAdultsNum.addEventListener("change", calculateAdvCost);
+
+// Check the number of foreign kids and update the total 
+advforeignKidsNum.addEventListener("change", calculateAdvCost);
+
+// Handle the adventure book now button
+advBookBtn.addEventListener("click", () => {
+    calculateAdvCost();
+
+    // Validate user input
+    if (isAdvFormValidated()) {
+        // Create adventure booking summary and add to the html
+        advOutputSummary.innerHTML = createAdvSummary();
+        // Display pop up window
+        advConfirmPopup.style.transform = `none`;
+        bluredBackground.style.display = 'block';
+    }
+});
+
+// Close adventure booking summary pop up window
+advPopupClose.addEventListener("click", () => {
+    // Hide popup window
+    advConfirmPopup.style.transform = `translateY(100%) scale(0)`;
+    bluredBackground.style.display = 'none';
+    // Reset the form after user close the popup window
+    advBooking_form.reset();
+    initialliseAdvForm();
+});
+
+// Store adventure booking data in the local storage
+addToFavAdv.addEventListener("click", storeAdvBookingData);
+
+// Apply adventure booking data to the form by getting data from the local storage
+advApplyFav.addEventListener("click", applyAdvFavourites);
+
+
+// Calculate total cost of addventure booking
+function calculateAdvCost() {
+    advLocalAdults = parseInt(advLocalAdultsNum.value);
+    advLocalKids = parseInt(advLocalKidsNum.value);
+    advForeignAdults = parseInt(advforeignAdultsNum.value);
+    advForeignKids = parseInt(advforeignKidsNum.value)
+    advTotalCost = advLocalAdults * localAdultsPrice + advLocalKids * localKidsPrice + advForeignAdults * foreignAdultsPrice + advForeignKids * foreignKidsPrice;
+    advCostDisplay.innerText = advTotalCost;
+    console.log(advTotalCost);
+};
+
+// Validate adventure booking form user inputs
+function isAdvFormValidated() {
+    let validated = false;
+
+    let userName = advBookingName.value;
+    let userNumber = advBookingPhone.value;
+    let timeSlot = validateTimeSlot();
+    let guests = parseInt(advLocalAdultsNum.value) + parseInt(advLocalKidsNum.value) + parseInt(advforeignAdultsNum.value) + parseInt(advforeignKidsNum.value);
+    let date = advDate.value;
+
+    // Checking null data to validate user input
+    if (!(userName)) {
+        alert("Please enter your Name");
+    } else if (!(userNumber)) {
+        alert("Please enter your mobile number");
+    } else if (!(timeSlot)) {
+        alert("Please select a time slot")
+    } else if (guests <= 0) {
+        alert("Please enter number of Guests");
+    } else if (!(date)) {
+        alert("Please enter the date");
+    } else {
+        validated = true;
+        console.log("Validation Done!");
+    }
+
+    // Return true if all the validations are passed else false
+    return validated;
+}
+
+// Validate adventure booking time slot
+function validateTimeSlot() {
+    let timeSelected = false;
+    // At least one time slot has to be checked to return true
+    advTime.forEach(element => {
+        if (element.checked) {
+            timeSelected = true;
+        }
+    })
+    return timeSelected;
+}
+
+// Create adventure booking summary
+function createAdvSummary() {
+    let summary = "";
+    // If the guide is selected add additional note at the end of the summary
+    if (guide) {
+
+        if (advLocalAdults > 0) {
+            summary += summaryTemplate("Local Adults", advLocalAdults, localAdultsPrice);
+        }
+
+        if (advLocalKids > 0) {
+            summary += summaryTemplate("Local Kids", advLocalKids, localKidsPrice);
+        }
+
+        if (advForeignAdults > 0) {
+            summary += summaryTemplate("Foreign Adults", advForeignAdults, foreignAdultsPrice);
+        }
+
+        if (advForeignKids > 0) {
+            summary += summaryTemplate("Foreign Kids", advForeignKids, foreignKidsPrice);
+        }
+        summary += `
+        <p>Adults have charged by Rs. 1000 extra for guide</p>
+        <p>Kids have charged by Rs. 500 extra for guide</p>`
+    }
+    else {
+
+        if (advLocalAdults > 0) {
+            summary += summaryTemplate("Local Adults", advLocalAdults, localAdultsPrice);
+        }
+
+        if (advLocalKids > 0) {
+            summary += summaryTemplate("Local Kids", advLocalKids, localKidsPrice);
+        }
+
+        if (advForeignAdults > 0) {
+            summary += summaryTemplate("Foreign Adults", advForeignAdults, foreignAdultsPrice);
+        }
+
+        if (advForeignKids > 0) {
+            summary += summaryTemplate("Foreign Kids", advForeignKids, foreignKidsPrice);
+        }
+    }
+
+    advCustomerName.innerText = `Name: ${advBookingName.value}`;
+    advDateOutput.innerText = advDate.value;
+    // Output the time slot selected
+    advTime.forEach(timeElement => {
+        switch (timeElement.value) {
+            case "9to10":
+                timeSlotOutput.innerText = "9.00 am - 10.00 am";
+                selectedAdvTime = timeElement.value;
+                break;
+            case "11to12":
+                timeSlotOutput.innerText = "11.00 am - 12.00 pm";
+                selectedAdvTime = timeElement.value;
+                break;
+            case "2to3":
+                timeSlotOutput.innerText = "2.00 pm - 3.00 pm";
+                selectedAdvTime = timeElement.value;
+                break;
+        }
+    });
+    advTotalOutput.innerText = "Rs. " + advTotalCost;
+
+    return summary;
+}
+
+// Template to create html elements to add on adventure booking summary
+function summaryTemplate(guestType, num, price) {
+    let summaryElement = "";
+
+    summaryElement = `
+    <div>
+        <p><span>${guestType} </span> x ${num}</p>
+        <p>Rs. <span>${num * price}</span></p>
+    </div>`
+
+    return summaryElement;
+}
+
+// Store favourite booking in local storage
+function storeAdvBookingData() {
+    // Store data as an object in the local storage
+    const bookingData = {
+        customerName: advBookingName.value,
+        customerPhone: advBookingPhone.value,
+        timeSlot: selectedAdvTime,
+        localAdults: advLocalAdults,
+        localKids: advLocalKids,
+        foreignAdults: advForeignAdults,
+        foreignKids: advForeignKids,
+        date: advDate.value,
+        guide: guide
+    }
+    localStorage.setItem("advBooking", JSON.stringify(bookingData));
+}
+
+// Apply favourites to the booking
+function applyAdvFavourites() {
+    // Getting data from the local storage
+    const bookingData = JSON.parse(localStorage.getItem('advBooking'));
+
+    // Check if the local stroage has the needed value
+    if (!(Object.is(bookingData, null))) {
+        advBookingName.value = bookingData.customerName;
+        advBookingPhone.value = bookingData.customerPhone;
+        advLocalAdultsNum.value = bookingData.localAdults;
+        advLocalKidsNum.value = bookingData.localKids;
+        advforeignAdultsNum.value = bookingData.foreignAdults;
+        advforeignKidsNum.value = bookingData.foreignKids;
+        advDate.value = bookingData.date;
+
+        const timeSelected = bookingData.timeSlot;
+        const guideSelected = bookingData.guide;
+
+
+        // Select time slot from favourites
+        advTime.forEach(element => {
+            if (element.value == timeSelected) {
+                element.checked = true;
+            }
+        });
+
+        // Selecte guide choice from favourites
+        guideSelection.forEach(element => {
+            // Select with guide
+            if (element.value == "withGuide" && guideSelected) {
+                element.checked = true;
+            }
+            // Additional (Select with out guide)
+            if (element.value == "withoutGuide" && !guideSelected) {
+                element.checked = true;
+            }
+        })
+
+        calculateAdvCost();
+    } else alert("You haven't saved any favourites!");
+    // trow an alert if the value is not saved in the local storage
 }
